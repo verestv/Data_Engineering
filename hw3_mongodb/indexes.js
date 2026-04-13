b58b16b64b3fb7f8229c48785900140af1999b94
Verestiuk  Ivan <94048443+verestv@users.noreply.github.com>
2026-04-13 15:26:15 +0300

    Delete hw3_mongodb directory

--- hw3_mongodb/indexes.js
+++
@@ -1,4 +0,0 @@
-db.user_engagements.createIndex({ "impressions.timestamp": 1 }, { name: "idx_impressions_timestamp" })
-db.user_engagements.createIndex({ "impressions.campaign_id": 1 }, { name: "idx_impressions_campaign" })
-db.user_engagements.createIndex({ "impressions.advertiser_name": 1 }, { name: "idx_impressions_advertiser" })
-db.user_engagements.createIndex({ "impressions.campaign_name": 1 }, { name: "idx_impressions_campaign_name" })
